ebfcc6ec6b60266b7f92e1390bf58f6f0b3428ca
Setareh Hojjaty Shargh <fhojjaty00@gmail.com>
2025-12-29 20:35:26 +0330

    feat: add database models and relationships

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.director import Director  # noqa: F401
+from app.models.genre import Genre  # noqa: F401
+from app.models.movie import Movie  # noqa: F401
+from app.models.movie_rating import MovieRating  # noqa: F401
+from app.models.association import genres_movie  # noqa: F401
